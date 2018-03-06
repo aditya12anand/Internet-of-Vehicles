ddb178859959e7e6eec8ad4b0dc4ea43fae1ae57
Risabh Mishra <rishabh.mshr@yahoo.in>
2018-03-06 19:24:29 +0530

    IoV

--- IoV.ino
+++ IoV.ino
@@ -10,7 +10,39 @@ SoftwareSerial gpsSerial(D7,D6);//rx,tx
 TinyGPS gps; // create gps object 
 
 #define dht_dpin 0
-DHT dht(dht_dpin, DHTTYPE); 
+DHT dht(dht_dpin, DHTTYPE);
+
+int trigPin = D3;
+int echoPin = D4;
+
+#include <Wire.h>
+
+// MPU6050 Slave Device Address
+const uint8_t MPU6050SlaveAddress = 0x68;
+
+// Select SDA and SCL pins for I2C communication 
+const uint8_t scl = D6;
+const uint8_t sda = D7;
+
+// sensitivity scale factor respective to full scale setting provided in datasheet 
+const uint16_t AccelScaleFactor = 16384;
+const uint16_t GyroScaleFactor = 131;
+
+// MPU6050 few configuration register addresses
+const uint8_t MPU6050_REGISTER_SMPLRT_DIV   =  0x19;
+const uint8_t MPU6050_REGISTER_USER_CTRL    =  0x6A;
+const uint8_t MPU6050_REGISTER_PWR_MGMT_1   =  0x6B;
+const uint8_t MPU6050_REGISTER_PWR_MGMT_2   =  0x6C;
+const uint8_t MPU6050_REGISTER_CONFIG       =  0x1A;
+const uint8_t MPU6050_REGISTER_GYRO_CONFIG  =  0x1B;
+const uint8_t MPU6050_REGISTER_ACCEL_CONFIG =  0x1C;
+const uint8_t MPU6050_REGISTER_FIFO_EN      =  0x23;
+const uint8_t MPU6050_REGISTER_INT_ENABLE   =  0x38;
+const uint8_t MPU6050_REGISTER_ACCEL_XOUT_H =  0x3B;
+const uint8_t MPU6050_REGISTER_SIGNAL_PATH_RESET  = 0x68;
+
+int16_t AccelX, AccelY, AccelZ, Temperature, GyroX, GyroY, GyroZ;
+
 
 #define FIREBASE_HOST "minor-project-70169.firebaseio.com"
 #define FIREBASE_AUTH "2x1Gi4tWv53dNo0yKxuMVyslznTpZsqhQR8jALGm"
@@ -21,7 +53,14 @@ void setup(void)
 { 
   dht.begin();
   Serial.begin(9600);
+
+pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
+pinMode(echoPin, INPUT); // Sets the echoPin as an Input
+  
 gpsSerial.begin(9600); // connect gps sensor 
+
+Wire.begin(sda, scl);
+  MPU6050_Init();
 
 WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
   Serial.print("connecting");
@@ -34,7 +73,7 @@ WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
   Serial.println(WiFi.localIP());
   Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
 
-  Serial.println("Humidity and temperature\n\n");
+  //Serial.println("Humidity and temperature\n\n");
   delay(700);
 
 }
@@ -48,8 +87,8 @@ void loop() {
     Serial.print(t); 
     Serial.println("C  ");
 
-    Firebase.pushFloat("t&h/temperature", t);
-    Firebase.pushFloat("t&h/humidity", h);
+    //Firebase.pushFloat("t&h/temperature", t);
+    //Firebase.pushFloat("t&h/humidity", h);
 
 
     while(gpsSerial.available()){ // check for gps data 
@@ -73,12 +112,94 @@ Serial.println(latitude+";"+longitude);
     //Firebase.pushString("gps/Latitude", latitude);
     //Firebase.pushString("gps/Longiitude", longitude);
 
+
+digitalWrite(trigPin, LOW);
+delayMicroseconds(2);
+// Sets the trigPin on HIGH state for 10 micro seconds
+digitalWrite(trigPin, HIGH);
+delayMicroseconds(1000);
+digitalWrite(trigPin, LOW);
+// Reads the echoPin, returns the sound wave travel time in microseconds
+float duration = pulseIn(echoPin, HIGH);
+// Calculating the distance
+float distance = (duration*0.034)/2 ;
+Firebase.pushFloat("front_us",distance);
+
+
+double Ax, Ay, Az, T, Gx, Gy, Gz;
+  
+  Read_RawValue(MPU6050SlaveAddress, MPU6050_REGISTER_ACCEL_XOUT_H);
+  
+  //divide each with their sensitivity scale factor
+  Ax = (double)AccelX/AccelScaleFactor;
+  Ay = (double)AccelY/AccelScaleFactor;
+  Az = (double)AccelZ/AccelScaleFactor;
+  T = (double)Temperature/340+36.53; //temperature formula
+  Gx = (double)GyroX/GyroScaleFactor;
+  Gy = (double)GyroY/GyroScaleFactor;
+  Gz = (double)GyroZ/GyroScaleFactor;
+
+  Serial.print("Ax: "); Serial.print(Ax);
+  Serial.print(" Ay: "); Serial.print(Ay);
+  Serial.print(" Az: "); Serial.print(Az);
+  Serial.print(" T: "); Serial.print(T);
+  Serial.print(" Gx: "); Serial.print(Gx);
+  Serial.print(" Gy: "); Serial.print(Gy);
+  Serial.print(" Gz: "); Serial.println(Gz);
+
+   Firebase.pushFloat("A&G/Ax",Ax);
+   Firebase.pushFloat("A&G/Ay",Ay);
+   Firebase.pushFloat("A&G/Az",Az);
+   Firebase.pushFloat("A&G/T",T);
+   Firebase.pushFloat("A&G/Gx",Gx);
+   Firebase.pushFloat("A&G/Gy",Gy);
+   Firebase.pushFloat("A&G/Gz",Gz);
+
+    
+
     if (Firebase.failed()) {
         Serial.print("setting /number failed:");
         Serial.println(Firebase.error());
         return;
     }
-  delay(1000);
+  delay(100);
+}
+
+void I2C_Write(uint8_t deviceAddress, uint8_t regAddress, uint8_t data){
+  Wire.beginTransmission(deviceAddress);
+  Wire.write(regAddress);
+  Wire.write(data);
+  Wire.endTransmission();
+}
+
+// read all 14 register
+void Read_RawValue(uint8_t deviceAddress, uint8_t regAddress){
+  Wire.beginTransmission(deviceAddress);
+  Wire.write(regAddress);
+  Wire.endTransmission();
+  Wire.requestFrom(deviceAddress, (uint8_t)14);
+  AccelX = (((int16_t)Wire.read()<<8) | Wire.read());
+  AccelY = (((int16_t)Wire.read()<<8) | Wire.read());
+  AccelZ = (((int16_t)Wire.read()<<8) | Wire.read());
+  Temperature = (((int16_t)Wire.read()<<8) | Wire.read());
+  GyroX = (((int16_t)Wire.read()<<8) | Wire.read());
+  GyroY = (((int16_t)Wire.read()<<8) | Wire.read());
+  GyroZ = (((int16_t)Wire.read()<<8) | Wire.read());
+}
+
+//configure MPU6050
+void MPU6050_Init(){
+  delay(150);
+  I2C_Write(MPU6050SlaveAddress, MPU6050_REGISTER_SMPLRT_DIV, 0x07);
+  I2C_Write(MPU6050SlaveAddress, MPU6050_REGISTER_PWR_MGMT_1, 0x01);
+  I2C_Write(MPU6050SlaveAddress, MPU6050_REGISTER_PWR_MGMT_2, 0x00);
+  I2C_Write(MPU6050SlaveAddress, MPU6050_REGISTER_CONFIG, 0x00);
+  I2C_Write(MPU6050SlaveAddress, MPU6050_REGISTER_GYRO_CONFIG, 0x00);//set +/-250 degree/second full scale
+  I2C_Write(MPU6050SlaveAddress, MPU6050_REGISTER_ACCEL_CONFIG, 0x00);// set +/- 2g full scale
+  I2C_Write(MPU6050SlaveAddress, MPU6050_REGISTER_FIFO_EN, 0x00);
+  I2C_Write(MPU6050SlaveAddress, MPU6050_REGISTER_INT_ENABLE, 0x01);
+  I2C_Write(MPU6050SlaveAddress, MPU6050_REGISTER_SIGNAL_PATH_RESET, 0x00);
+  I2C_Write(MPU6050SlaveAddress, MPU6050_REGISTER_USER_CTRL, 0x00);
 }
 
 
